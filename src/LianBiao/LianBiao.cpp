3d9d8c25481171e8aff5febfc11c00d6a5c0054e
郑汉荣 <hanrongzheng@gmail.com>
2016-07-06 23:33:34 +0800

    Normal commit

--- src/LianBiao/LianBiao.cpp
+++ src/LianBiao/LianBiao.cpp
@@ -1,4 +1,4 @@
-using namespace std;
+﻿using namespace std;
 #include <iostream>
 #include "stdlib.h"
 
@@ -8,24 +8,29 @@ struct LianBiao {
     LianBiao* next;
 };
 
-void init(LianBiao* head) {
-    head = (LianBiao*) malloc (sizeof(LianBiao));
-}
-
 void set(int* array, LianBiao* head) {
-    LianBiao* last;
-    LianBiao* cur;
+    
     // 第一个元素
-    last = (LianBiao*) malloc (sizeof(LianBiao));
-    head->next = last;
-    int size = sizeof(array)/sizeof(array[0]);
-    cout<<size;
-    for (int i = 0; i < size; i++) {
+    LianBiao* last = (LianBiao*) malloc (sizeof(LianBiao));
+    LianBiao* cur = last;
+    cout<<array[0]<<"\t";
+    last->data = array[0];
+    head = last;
+    int size = 9;
+    // 从一开始，减去第一个元素
+    for (int i = 1; i < size; i++) {
         cur = (LianBiao*) malloc (sizeof(LianBiao));
-	cur->data = array[i];
-	last->next = cur;
-	last = cur;
+		cout<<array[i]<<"\t";
+		cur->data = array[i];
+		last->next = cur;
+		last = cur;
     }
+	cur = head;
+	while (cur != NULL) {
+		cout<<cur->data<<"\t";
+		cur = cur->next;
+	}
+    
     last = NULL;
     cur = NULL;
     cout<<"\n";
@@ -34,16 +39,15 @@ void set(int* array, LianBiao* head) {
 void get(LianBiao* head) {
     LianBiao* iterator = head;
     while (iterator != NULL) {
-        cout<<iterator->data;
-	iterator = iterator->next;
+        cout<<iterator->data<<"\t";
+		iterator = iterator->next;
     }
     cout<<"\n";
 }
 
 int main() {
-    LianBiao* head;
-    init(head);
-    int a[] = {1,3,5,6,7,3,5,7,1};
+    struct LianBiao* head = (LianBiao*) malloc (sizeof(LianBiao));
+    int a[9] = {1,3,5,6,7,3,5,7,1};
     set(&a[0], head);
     get(head);
     return 0;
